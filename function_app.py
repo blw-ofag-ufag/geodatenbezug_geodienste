caee335a8a05804049bc639ae3576975fde867a3
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2024-03-22 14:34:39 +0100

    Fix linter

--- function_app.py
+++ function_app.py
@@ -7,6 +7,8 @@ app = func.FunctionApp()
 @app.schedule(
     schedule="0 */1 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False
 )
+# myTimer is defined by azure functions and cannot be changed
+# pylint: disable-next=invalid-name
 def get_derivates_from_geodienste(myTimer: func.TimerRequest) -> None:
     """Timer trigger function which gets the derivatives from the geodienste.de API every 5 minutes."""
     if myTimer.past_due:
