644a55115cc504fddc7ec609462b4c48b9a3fd08
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2024-03-27 10:54:53 +0100

    Add configuration to use custom schedule on portal

--- function_app.py
+++ function_app.py
@@ -1,10 +1,16 @@
+import os
 import logging
 import azure.functions as func
 
 app = func.FunctionApp()
 
 
-@app.schedule(schedule="0 */1 * * * *", arg_name="timer", run_on_startup=True, use_monitor=False)
+@app.schedule(
+    schedule=os.environ["TimeTriggerSchedule"],
+    arg_name="timer",
+    run_on_startup=True,
+    use_monitor=False,
+)
 def trigger_retrieve_geodata(timer: func.TimerRequest) -> None:
     """Timer trigger function which starts the retrieval of geodata from geodienste.ch."""
     if timer.past_due:
