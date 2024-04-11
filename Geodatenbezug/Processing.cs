44c7d83a4d0e7cf8bfcd3325e40c18e83a43601c
Roswita Tschümperlin <108136714+tschumpr@users.noreply.github.com>
2024-04-11 16:04:11 +0200

    Remove unnecessary brackets
    Co-authored-by: Daniel Jovanovic <daniel.jovanovic@geowerkstatt.ch>

--- Geodatenbezug/Processing.cs
+++ Geodatenbezug/Processing.cs
@@ -8,7 +8,7 @@ public class Processing(IGeodiensteApi geodiensteApi, ILogger<Processing> logger
     {
         var topics = await geodiensteApi.RequestTopicInfoAsync();
         var currentTime = DateTime.Now;
-        var topicsToProcess = topics.FindAll((topic) =>
+        var topicsToProcess = topics.FindAll(topic =>
         {
             if (topic.UpdatedAt != null)
             {
