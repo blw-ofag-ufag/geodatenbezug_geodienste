6ca21a8869ca4c8f69bf0cafc7d9b70da63328cd
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2024-04-12 16:34:19 +0200

    Fix loglevel setup

--- Geodatenbezug.Test/TestExtensions.cs
+++ Geodatenbezug.Test/TestExtensions.cs
@@ -8,7 +8,7 @@ internal static class TestExtensions
     internal static void Setup<T>(this Mock<ILogger<T>> loggerMock, LogLevel expectedLogLevel, string expectedLogSubstring)
         => loggerMock
             .Setup(l => l.Log(
-                It.IsAny<LogLevel>(),
+                expectedLogLevel,
                 It.IsAny<EventId>(),
                 It.Is<It.IsAnyType>((o, t) => o.ToString().Contains(expectedLogSubstring, StringComparison.OrdinalIgnoreCase)),
                 It.IsAny<Exception>(),
