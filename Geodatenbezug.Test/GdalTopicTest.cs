ee7a5a31b761eeb0a532ae3443189fd1158507a7
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2024-04-18 17:13:55 +0200

    Add debug tests

---
+++ Geodatenbezug.Test/GdalTopicTest.cs
@@ -0,0 +1,49 @@
+﻿using Geodatenbezug.Topics;
+
+namespace Geodatenbezug;
+
+[TestClass]
+public class GdalTopicTest
+{
+/*    [TestMethod]
+    public void RebbaukatasterTest()
+    {
+        var result = new Rebbaukataster("C:\\Users\\rtschuemperlin\\Desktop\\BLW\\data\\BE\\lwb_rebbaukataster_v2_0_lv95_konform.gpkg").Process();
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [TestMethod]
+    public void PerimeterTerrassenrebenTest()
+    {
+        var result = new PerimeterTerrassenreben("C:\\Users\\rtschuemperlin\\Desktop\\BLW\\data\\BE\\lwb_perimeter_terrassenreben_v2_0_lv95_konform.gpkg").Process();
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [TestMethod]
+    public void PerimeterLnSfTest()
+    {
+        var result = new PerimeterLnSf("C:\\Users\\rtschuemperlin\\Desktop\\BLW\\data\\BE\\lwb_perimeter_ln_sf_v2_0_lv95_konform.gpkg").Process();
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [TestMethod]
+    public void NutzungsflaechenTest()
+    {
+        var result = new Nutzungsflaechen("C:\\Users\\rtschuemperlin\\Desktop\\BLW\\data\\BE\\lwb_nutzungsflaechen_v2_0_lv95_konform.gpkg").Process();
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [TestMethod]
+    public void BiodiversitaetsfoerderflaechenTest()
+    {
+        var result = new Biodiversitaetsfoerderflaechen("C:\\Users\\rtschuemperlin\\Desktop\\BLW\\data\\BE\\lwb_biodiversitaetsfoerderflaechen_v2_0_lv95_konform.gpkg").Process();
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [TestMethod]
+    public void BewirtschaftungseinheitTest()
+    {
+        var result = new Bewirtschaftungseinheit("C:\\Users\\rtschuemperlin\\Desktop\\BLW\\data\\BE\\lwb_bewirtschaftungseinheit_v2_0_lv95_konform.gpkg").Process();
+        Assert.AreEqual(string.Empty, result);
+    }*/
+}
